9826c8b1f455e71fd53268ec029841b500288be5
Federico Garcea <v-fedgar@microsoft.com>
2021-05-26 08:38:32 +0200

    Aggiorna stile e presentazione dei Contenuti, con coordinate sotto il titolo, font-weight normale, e usa margin invece di hr per separare le sezioni.

--- clientapp/static/js/views/PlaceView.js
+++ clientapp/static/js/views/PlaceView.js
@@ -23,29 +23,22 @@ export default class extends AbstractView {
     if (response.status >= 200 && response.status < 400) {
 
       html += "<div class='titolo'>" + item.name + "</div>";
-      //html += "<div class='container'>";
-      html += " <img class='img-fluid w-90' src='" + getImageUrl(item.imageUID) + "'></img>";
-      //html += "</div>";
 
-      html += "<hr></hr>";
+      html += "<div class='container coords'>";
+      html += "<p>lat: " + item.lat + " , long : " + item.long + "&nbsp;<i class='fas fa-map-marker-alt'></i></p>";
+      html += "</div>";
 
-      html += "<div class='container'>";
+      html += "<div class='container mb-2'>";
       html += item.intro_text;
       html += "</div>";
 
-      html += "<hr></hr>";
-
-      html += "<div class='container'>";
-      html += "<label>Posizione (coordinate)</label>lat: " + item.lat + " , long : " + item.long;
+      html += "<div class='container mb-2'>";
+      html += " <img class='img-fluid w-90' src='" + getImageUrl(item.imageUID) + "'></img>";
       html += "</div>";
 
-      html += "<hr></hr>";
-
-      html += "<div class='container'>";
+      html += "<div class='container mb-2'>";
       html += item.full_text;
       html += "</div>";
-
-      html += "<hr></hr>";
 
       if (item.audio) {
         html += "<div class='container-fluid'>";
@@ -53,19 +46,16 @@ export default class extends AbstractView {
         html += " <iframe width='100%' height='60' src='" + item.audio +"'></iframe>";
         html += "</div>";
       }
-      html += "<hr></hr>";
 
       if (item.video) {
-        html += "<div class='embed-responsive embed-responsive-16by9'>";
+        html += "<div class='embed-responsive embed-responsive-16by9 mb-2'>";
         html += "<iframe class='embed-responsive-item' src='" + item.video + "'></iframe>";
         html += "</div>";
       }
 
-      html += "<hr></hr>";
-      html += "<div class='container'>";
+      html += "<div class='container mb-2'>";
       html += "<label>Tags: </label>&nbsp;" + item.tags;
       html += "</div>";
-      html += "<hr></hr>";
       html += "<div class='mx-auto text-center'><a class='btn btn-primary' href='" + u('/categorie/' + item.category_id) + "' data-link>Altri contenuti</a></div>";
       
       // close main container
