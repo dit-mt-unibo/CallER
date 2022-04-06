522e63fff9ee53ee5817778ea11eb3c43782e306
Francesco <francescosanti123@gmail.com>
2022-04-06 19:07:06 +0200

    parola del giorno

--- webapp/src/components/home.page.vue
+++ webapp/src/components/home.page.vue
@@ -3,27 +3,35 @@
 <template>
     <toolbar :title="''" :category_id="''" />
     <div class="container pt-4">
-        <div align="center" class="row">            
+        <div align="center" class="row">
             <div class="col-6 col-md-4" v-for="category in categories" v-bind:key="category.id" v-bind:category="category">
                 <div class="home-icon-box" align="center">
                     <span class="btn" @click="goTo(category.id)">
                         <i class="fas fa-3x" :class="'fa-' + category.icon"></i>
                         <p class="mt-3">{{category.name}}</p>
                     </span>
-                </div>             
-            </div>            
+                </div>
+            </div>
         </div> <!-- chiusura row -->
-    </div> <!-- chiusura container -->    
+         <!-- Parola del giorno -->
+        <div id="popUp">
+           <glossary/>
+        </div>
+          <!-- chiusura parola del giorno -->
+    </div> <!-- chiusura container -->
 </template>
 
 <script>
 
 import toolbar from './toolbar.component.vue';
+import  glossary  from './parola-del-giorno.component.vue'
 
 const axios = require('axios')
+//Funzione che dopo 5 secondi rimuove dallo schermo il componete della parola del giorno
+setTimeout(function(){document.getElementById('popUp').style.display="none"}, 7000);
 
 export default {
-    
+
     name: 'home',
 
     // Utilizza variabile globale apiUrl
@@ -34,41 +42,40 @@ export default {
         return {
             // Oggetto categorie principali recuperate dal database
             categories: [],
-        }        
+        }
+    },
+
+    created : async function() {
+
+        await this.getCategories();
 
     },
-    
-    created : async function() {
-    
-        await this.getCategories();
-    
-    },
-    
+
     methods : {
-        
+
         /**
          * Recupera le categorie principali dal database e popola l'oggetto this.categories.
          * La categoria Home ID = 1 è esclusa dall'elenco
          */
         async getCategories() {
-            
+
             const response = await axios.get(this.apiUrl + "/category");
-            
+
             response.data.forEach(element => {
-                
+
                 if ( element.id != 1 && element.parent_id == null ) {
-                    
+
                     this.categories.push(element);
 
                 }
 
-            });      
-            
+            });
+
         },
 
         /**
          * Listener evento click. Apre la pagina con l'elenco delle sottocategorie
-         * 
+         *
          * @param {int} category_id ID categoria
          */
         goTo(category_id) {
@@ -77,13 +84,14 @@ export default {
             this.$router.push(url);
 
         }
-        
+
     },
 
     components: {
-        toolbar
+        toolbar,
+        glossary
     }
-    
+
 }
 
-</script>    
+</script>
---
+++ webapp/src/components/parola-del-giorno.page.vue
@@ -0,0 +1,22 @@
+<template><div id="parola">
+<glossary></glossary>
+<!-- <div><a href="https://www.dizionario-italiano.it/dizionario-italiano.php?parola=spaghetti">Link al dizionario</a></div> -->
+</div>
+
+</template>
+
+<script>
+import  glossary  from './parola-del-giorno.component.vue'
+// let par = "";
+// const par = setTimeout(function(){document.getElementById('parola')}, 50);
+
+// console.log(prova)
+ export default {
+   name: 'parola',
+
+   components: {
+        glossary
+    },
+  glossary
+ }
+</script>
