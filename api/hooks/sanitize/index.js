b4a7c3d91ffbef213e21b801409195ef06fe1cfd
daniele-ng <daniele.negretti2@gmail.com>
2022-05-17 12:44:11 +0200

    creato hook sails per la pulizia delle stringhe inviate dall'utente

---
+++ api/hooks/sanitize/index.js
@@ -0,0 +1,77 @@
+/**
+ * Hook Sanitize
+ * 
+ * @description Hook for cleaning user input. 
+ */
+
+module.exports = function(){
+
+    const sanitizeHtml = require('sanitize-html');
+    const blackListWords = [];
+    const blackListChars = [];
+
+    return {
+
+        /**
+         * Removes html tags from string
+         * @param {string} text user input
+         * @returns string
+         */
+        cleanHtml: function(text){
+
+            return sanitizeHtml(text ,{
+                allowedTags: [],
+                allowedAttributes: {}
+            });
+
+        },
+
+        /**
+         * Removes blacklisted words from string
+         * @param {string} text user input
+         * @returns string
+         */
+        cleanWords: function(text){
+
+            blackListWords.forEach(word => {
+                
+                let pattern = new RegExp(word , "g");
+                text = text.replace(pattern , "");
+
+            });
+
+            return text;
+        },
+
+        /**
+         * Removes unwanted chars from string
+         * @param {string} text user input
+         * @returns string
+         */
+        cleanChars: function(text){
+
+            blackListChars.forEach(char => {
+
+                let pattern = new RegExp("[" + char + "]", "g");
+                text = text.replace(pattern , "");
+
+            });
+
+            return text;
+        },
+
+        /**
+         * Removes html tags, unwanted chars and blacklisted words from string
+         * @param {string} text user input
+         * @returns string
+         */
+        deepClean: function(text){
+
+            text = this.cleanHtml(text);
+            text = this.cleanWords(text);
+            text = this.cleanChars(text);
+
+            return text;
+        }
+    }
+}
