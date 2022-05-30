53f8f7e72c514105f4801a5024d572aaab8cbdba
daniele-ng <daniele.negretti2@gmail.com>
2022-05-30 17:59:21 +0200

    lato client nella pagina glossario aggiunto box per l'immagine e pulsante ascolto audio

--- webapp/src/components/glossary.page.vue
+++ webapp/src/components/glossary.page.vue
@@ -4,11 +4,28 @@
     <div class="container">
         <toolbar :title="'Glossario'" :category_id="''" />
         <div class="row mb-2">
-            <div class="col-12 titolo">{{item.term}}</div>
+            <div class="col-12 titolo">
+                <div style="display: inline-block; vertical-align:middle;">{{item.term}}</div>
+                <div v-if="item.audio" class="audio-player-box" @click="playAudio()">
+                    <i id="speaker" class="audio-player-volume fas fa-volume-off"></i>
+                    <span class="audio-player-label">ascolta</span>
+                </div>                
+            </div>            
         </div>
         <div class="row rounded-top place-content pt-3">
-            <div class="col-12">
-                <p v-html="item.definition"></p>
+            <div v-if="item.term" class="col-12">
+                <div v-if="item.image" class="box-place-img">
+                    <div class="place-container-img">
+                        <img class="place-img" :src="imagePath" />
+                    </div>
+                    <p id="caption" class="caption card-text-truncate" @click="expand">
+                        {{ item.image_caption }}
+                    </p>
+                </div>
+                <span v-html="item.definition"></span>
+            </div>
+            <div v-if="!item.term" class="col-12">
+                <p>Nessun vocabolo disponibile</p>
             </div>
         </div>
         <div class="row mt-4 mb-4">
@@ -38,12 +55,61 @@ export default {
         return {
 
             //oggetto parola del giorno. Dati recuperati tramiti chiamata API
-            item: {}
+            item: {},
+            // oggetto audio
+            audioElement: null,
 
         }
     },
 
+    watch: {
+
+        "item" () {
+
+            if (typeof this.item.audioUID === "undefined" || this.item.audioUID == null) {
+
+                this.audioElement.src = "";
+
+            }
+            else{
+
+                this.audioElement.src = this.apiUrl + "/glossario/" + this.item.audioUID;
+
+            }            
+
+        } 
+
+    },
+
+    computed: {
+
+        imagePath() {
+            
+            if (typeof this.item.imageUID === "undefined" || this.item.imageUID == null) return "";
+
+            return this.apiUrl + "/glossario/" + this.item.imageUID;
+        },
+
+    },
+
     beforeMount: async function(){
+
+        // Inizializza oggetto audio
+        this.audioElement = new Audio();
+
+        // Listener evento play su oggetto audio
+        this.audioElement.addEventListener('play' , () => {
+            
+            document.getElementById("speaker").classList.replace("fa-volume-off" , "fa-volume-up");
+
+         });
+
+         // Listener evento pause su oggetto audio
+        this.audioElement.addEventListener('pause' , () => {
+            
+            document.getElementById("speaker").classList.replace("fa-volume-up" , "fa-volume-off");
+
+         });
 
         await this.getTodaysWord();
         
@@ -68,10 +134,24 @@ export default {
 
         },
 
+        /**
+         * Chiamata API, recupera una parola a caso dal glossario
+         */
         async getRandomWord() {
 
             const response = await axios.get(this.apiUrl + '/api/get-random-word');
-            this.item = response.data.item
+            this.item = response.data.item;
+
+        },
+
+        /**
+         * Listener evento click bottone play audio.
+         * Avvia riproduzione audio. 
+         */
+        playAudio() {
+
+            this.audioElement.play();
+
         }
 
     },
